18532597206d047f940fb7fe04ab4a210a699944
Caine J Nielsen <caine.lloyd.nielsen@gmail.com>
2019-03-01 20:35:48 -0700

    set up database with firebase

--- index.js
+++ index.js
@@ -1,77 +1,39 @@
-var data = [
-    {
-      name: 'Nexus',
-      theme: 'light',  
-      lists: [ // 0
-        {type: 'action', text: 'Quick Access', link: '', action: 1},
-        {type: 'action', text: 'Test', link: '', action: 2},
-        {type: 'action', text: 'Google', link: '', action: 3},
-        {type: 'action', text: 'Software Development', link: '', action: 4}        
-      ]
-    },
-    {
-      name: 'Quick Access',
-      theme: 'light',  
-      lists: [ // 1
-        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
-        {type: 'link', text: 'Firebase Console',link: 'https://google.com'},
-        {type: 'link', text: 'Github',link: 'https://github.com/'},
-        {type: 'link', text: 'CodePen',link: 'https://codepen.io/dashboard/'}
-      ]
-    },
-    {
-      name: 'Test',
-      theme: 'light',  
-      lists: [ // 2
-        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
-        {type: 'link', text: 'Google',link: 'https://google.com'},
-        {type: 'link', text: 'Google',link: 'https://google.com'},
-        {type: 'link', text: 'Google',link: 'https://google.com'}
-      ]
-    },
-    {
-      name: 'Google',
-      theme: 'light',  
-      lists: [ // 3
-      {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
-        {type: 'link', text: 'Google Drive',link: 'https://drive.google.com'},
-        {type: 'link', text: 'Google Docs',link: 'https://docs.google.com'},
-        {type: 'link', text: 'Google Sheets',link: 'https://sheets.google.com'},
-        {type: 'link', text: 'Google Slides',link: 'https://slides.google.com'}
-      ]
-    },
-    {
-      name: 'Software Development',
-      theme: 'light',  
-      lists: [ // 4
-        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
-        {type: 'link', text: 'caineandrebekah.com',link: 'https://caineandrebekah.com'},
-        {type: 'link', text: 'docs.caineandrebekah.com',link: 'https://docs.caineandrebekah.com'},
-        {type: 'link', text: 'Google Firebase Console',link: 'https://console.firebase.google.com'},
-        {type: 'link', text: 'Google Cloud Console',link: 'https://console.cloud.google.com/home/dashboard'},
-        {type: 'link', text: 'Github',link: 'https://google.com'},
-        {type: 'link', text: 'CodePen',link: 'https://google.com'}
-      ]
-    }
-  ];
+// INTRO
   
   var list = document.getElementById('pageList');
   var defaultFavicon = 'http://s2.googleusercontent.com/s2/favicons?domain_url=https://caineandrebekah.com'
   var db = firebase.firestore();
-  var presentedCode = window.location.query;
-  console.log(presentedCode);
 
-  db.collection("lists").where("statecode", "==", presentedCode).then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      console.log(doc.data());
-    });
-  });
+  var searches = window.location.search.split("?");
+  search = searches[1].split("&");
+  searchName = search[0].split("=");
+
+// LOAD LISTS FROM THE DATABASE
+
+  window.onload = function collectData() {
+    if (searchName[0] == "list") {
+
+      collection = db.collection("lists");
+
+      var result = collection.where("namecode", "==", searchName[1]);
+
+      result.get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+          console.log(doc.id);
+          fillList(doc.data());
+        })
+      });
+      
+    }
+  }
+  
+// FILL THE LIST WITH LIST ITEMS  
   
   function fillList(data) {
+    console.log(data);
     var content = data.lists;
     document.getElementById('pageList').innerHTML = '';
-    document.getElementById('pageList').innerHTML += '<li id="list-header"><h1>' + data.name + '</h1></li>';
+    document.getElementById('pageList').innerHTML += '<li id="list-header-item"><h1>' + data.name + '</h1></li>';
     for(i = 0; i < content.length; i++) {
       if(content[i].type == 'link') {
         var favicon = 'http://s2.googleusercontent.com/s2/favicons?domain_url=' + content[i].link;
@@ -83,23 +45,103 @@ var data = [
         document.getElementById('pageList').innerHTML += actionItem;
       }
     }
-    document.getElementById('pageList').innerHTML += '<li id="create-button" onClick="createNew()"><p>+ Add</p></li>';
+    document.getElementById('pageList').innerHTML += '<li id="create-button-item" onClick="createNewListItem()"><p>+ Add</p></li>';
   }
-  
-  function createNew() {
-    document.getElementById("create-button").outerHTML = "";
-    document.getElementById('pageList').innerHTML += '<li id="create-menu"><div><input id="listItemLinkField" type="text" placeholder="https://website.com"></div><div><input id="listItemNameField" type="text" placeholder="Favorite Website"></div><div><span id="cancel" onClick="cancelNew()">Cancel</span><span id="save" onClick="saveNew()">Save</span></div></li>';
-  }
-  
-  function cancelNew() {
-    document.getElementById("create-menu").outerHTML = "";
-    document.getElementById('pageList').innerHTML += '<li id="create-button" onClick="createNew()"><p>+ Add</p></li>';
-  }
-  
-  function saveNew() {
 
+// MANAGING BUTTONS
+
+function showCancelAndSaveButtonItem() {
+  document.getElementById('pageList').innerHTML += '<li id="cancel-and-save-item"><div><span id="cancel" onClick="cancelNewListItem()">Cancel</span><span id="save" onClick="saveNewListItem()">Save</span></div></li>';
+}
+
+function hideCancelAndSaveButtonItem() {
+  document.getElementById('cancel-and-save-item').outerHTML = "";
+}
+
+function showCreateButtonItem() {
+  document.getElementById('pageList').innerHTML += '<li id="create-button-item" onClick="createNewListItem()"><p>+ Add</p></li>';
+}
+
+function hideCreateButtonItem() {
+  document.getElementById("create-button-item").outerHTML = "";
+}
+
+function showCreateNewItemItem() {
+  document.getElementById('pageList').innerHTML += '<li id="create-new-item-item"><input id="listItemLinkField" type="text" placeholder="https://website.com"></li>';
+}
+
+function hideCreateNewItemItem() {
+  document.getElementById("create-new-item-item").outerHTML = "";
+}
+
+// CREATING NEW LIST ITEMS
+  
+  function createNewListItem() {
+    hideCreateButtonItem();
+    showCreateNewItemItem();
+    showCancelAndSaveButtonItem();
+  }
+  
+  function cancelNewListItem() {
+    hideCreateNewItemItem();
+    hideCancelAndSaveButtonItem();
+    showCreateButtonItem();
+  }
+  
+  function saveNewListItem() {
     var savedListItem = {};
-    savedListItem.type = '';
+    savedListItem.type = 'link';
+    savedListItem.link = document.getElementById('listItemLinkField').value;
+    savedListItem.action = 0;
+
+    var savedList = {};
+
+    savedList.name = '';
+    savedList.namecode = '';
+    savedList.theme = '';
+    savedList.lists = [];
+    savedList.lists.push(savedListItem);
+
+    collection = db.collection("lists");
+
+    var result = collection.where("namecode", "==", searchName[1]);
+
+    console.log(result.doc);
+
+    result.update({
+        lists: firebase.firestore.FieldValue.arrayUnion("greater_virginia")
+    }).then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+
+    // db.collection("lists").add(savedList)
+    // .then(function(docRef) {
+    //   console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //   console.error("Error adding document: ", error);
+    // });
+    hideCreateNewItemItem();
+    hideCancelAndSaveButtonItem();
+    showCreateButtonItem();
+  }
+
+// CREATING NEW LISTS
+
+  function createNewList() {
+
+  }
+  
+  function cancelNewList() {
+
+  }
+
+  function saveNewList() {
+    var savedListItem = {};
+    savedListItem.type = 'link';
     savedListItem.text = document.getElementById('listItemNameField').value;
     savedListItem.link = document.getElementById('listItemLinkField').value;
     savedListItem.action = 0;
@@ -107,6 +149,7 @@ var data = [
     var savedList = {};
 
     savedList.name = '';
+    savedList.namecode = '';
     savedList.theme = '';
     savedList.lists = [];
     savedList.lists.push(savedListItem);
@@ -119,8 +162,6 @@ var data = [
       console.error("Error adding document: ", error);
     });
 
-    document.getElementById("create-menu").outerHTML = "";
-    document.getElementById('pageList').innerHTML += '<li id="create-button" onClick="createNew()"><p>+ Add</p></li>';
+    document.getElementById("create-new-item-item").outerHTML = "";
+    document.getElementById('pageList').innerHTML += '<li id="create-button-item" onClick="createNewListItem()"><p>+ Add</p></li>';
   }
-  
-  fillList(data[0]);
---
+++ olddata.js
@@ -0,0 +1,56 @@
+var data = [
+    {
+      name: 'Nexus',
+      theme: 'light',  
+      lists: [ // 0
+        {type: 'action', text: 'Quick Access', link: '', action: 1},
+        {type: 'action', text: 'Test', link: '', action: 2},
+        {type: 'action', text: 'Google', link: '', action: 3},
+        {type: 'action', text: 'Software Development', link: '', action: 4}        
+      ]
+    },
+    {
+      name: 'Quick Access',
+      theme: 'light',  
+      lists: [ // 1
+        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
+        {type: 'link', text: 'Firebase Console',link: 'https://google.com'},
+        {type: 'link', text: 'Github',link: 'https://github.com/'},
+        {type: 'link', text: 'CodePen',link: 'https://codepen.io/dashboard/'}
+      ]
+    },
+    {
+      name: 'Test',
+      theme: 'light',  
+      lists: [ // 2
+        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
+        {type: 'link', text: 'Google',link: 'https://google.com'},
+        {type: 'link', text: 'Google',link: 'https://google.com'},
+        {type: 'link', text: 'Google',link: 'https://google.com'}
+      ]
+    },
+    {
+      name: 'Google',
+      theme: 'light',  
+      lists: [ // 3
+      {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
+        {type: 'link', text: 'Google Drive',link: 'https://drive.google.com'},
+        {type: 'link', text: 'Google Docs',link: 'https://docs.google.com'},
+        {type: 'link', text: 'Google Sheets',link: 'https://sheets.google.com'},
+        {type: 'link', text: 'Google Slides',link: 'https://slides.google.com'}
+      ]
+    },
+    {
+      name: 'Software Development',
+      theme: 'light',  
+      lists: [ // 4
+        {type: 'action', text: '<i class="fas fa-angle-double-left"></i> Back', link: '', action: 0},
+        {type: 'link', text: 'caineandrebekah.com',link: 'https://caineandrebekah.com'},
+        {type: 'link', text: 'docs.caineandrebekah.com',link: 'https://docs.caineandrebekah.com'},
+        {type: 'link', text: 'Google Firebase Console',link: 'https://console.firebase.google.com'},
+        {type: 'link', text: 'Google Cloud Console',link: 'https://console.cloud.google.com/home/dashboard'},
+        {type: 'link', text: 'Github',link: 'https://google.com'},
+        {type: 'link', text: 'CodePen',link: 'https://google.com'}
+      ]
+    }
+  ];
